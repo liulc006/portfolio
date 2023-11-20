2b65030e27c62836b03304a79a79c2f09dee8737
Luca Liu <lucaliu96@gmail.com>
2023-11-20 17:50:51 -0500

    render deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,19 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
-  entry:'./server/index.js',
-  output: {
-    path: path.resolve('./build'),
-    filename: 'bundle.js'
-  },
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-react"],
+    devtool: "source-map",
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"],
+          },
         },
-      },
-      {
-        test: /\.png/,
-        type: "asset/resource",
-      },
-    ],
-  },
-  plugins: [
-    // New plugin
-    new HtmlWebpackPlugin({
-      // injects bundle.js to our new index.html
-      inject: true,
-      // copys the content of the existing index.html to the new /build index.html
-      template:  path.resolve('./static/index.html'),
-    }),
-  ]
-};
+        {
+          test: /\.png/,
+          type: "asset/resource",
+        },
+      ],
+    },
+  };
