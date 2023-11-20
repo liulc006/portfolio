4c9e0a75799bc8df0be25577d1cee6156b78a354
Luca Liu <lucaliu96@gmail.com>
2023-11-20 17:46:50 -0500

    webpack config for Netlify

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,37 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    devtool: "source-map",
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-react"],
-          },
+  entry:'./server/index.js',
+  output: {
+    path: path.resolve('./build'),
+    filename: 'bundle.js'
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-react"],
         },
-        {
-          test: /\.png/,
-          type: "asset/resource",
-        },
-      ],
-    },
-  };
+      },
+      {
+        test: /\.png/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    // New plugin
+    new HtmlWebpackPlugin({
+      // injects bundle.js to our new index.html
+      inject: true,
+      // copys the content of the existing index.html to the new /build index.html
+      template:  path.resolve('./static/index.html'),
+    }),
+  ]
+};
