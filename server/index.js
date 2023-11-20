9a291ff082707097cd69fecca48c028bdf1de9b7
Luca Liu <lucaliu96@gmail.com>
2023-11-20 11:40:07 -0500

    React+webpack Initial Setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
+// const cors = require('cors');
 
 const app = express();
+
+// app.use(cors());
+app.use('/dist', express.static(path.join(__dirname, '../dist')));
 
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '../src/index.html')));
 
