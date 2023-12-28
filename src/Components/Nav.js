437b09c6ff0eed4b6ba85539a1a672e2c7008fa5
Luca Liu <lucaliu96@gmail.com>
2023-12-28 00:13:32 -0500

    menu bar for mobile done

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,15 +1,19 @@
 import { Box, Typography, Button, Divider} from "@mui/material";
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 const Nav = () => {
+    const location = useLocation();
     const [ viewMenu, setViewMenu ] = useState(false);
 
+    //Close menu base whenever there is a hash change
     useEffect(()=>{
-        console.log(viewMenu)
-    }, [viewMenu])
+        setViewMenu(false)
+    }, [location.hash])
 
+    //handleing the open/close of the menu bar
     const handleMenu = (ev) => {
         ev.preventDefault();
         setViewMenu(!viewMenu);
@@ -49,12 +53,27 @@ const Nav = () => {
             </Box>
             {/* slide menu */}
             <Box sx={{display:{xs:'flex', md:'none'}, width:'100%', height:'100%', zIndex:'200', top:'0', right:(viewMenu ? "0" : "-100%"), position:'fixed', transition: '350ms'}}>
-                <Box sx={{width:'100%', height:'100%', backgroundColor:'secondary.main', opacity:'0.5', position:'absolute'}}/>
-                <Box sx={{width:'40%', height:'100%', backgroundColor:'secondary.main', opacity:'1', position:'absolute', zIndex:'200', top:'0', right:'0'}}>
-                    <h1>Slide Bar</h1>
-                    <Button onClick={(ev)=>{handleMenu(ev)}}>
-                            <MenuIcon />
-                    </Button>
+                <Box sx={{width:'100%', height:'100%', backgroundColor:'secondary.main', opacity:'0.5', position:'absolute'}} />
+                <Box sx={{display:'flex', flexDirection:'column', width:'60%', height:'100%', backgroundColor:'secondary.main', opacity:'1', position:'absolute', zIndex:'200', top:'0', right:'0'}}>
+                    <Box sx={{right:'1rem', display:'flex', height:'6rem', position:'absolute'}}>
+                        <Button onClick={(ev)=>{handleMenu(ev)}}>
+                                <CloseIcon />
+                        </Button>
+                    </Box>
+                    <Box sx={{display:'flex', flexDirection:'column', height:'100%', justifyContent:'center', alignItems:'center'}}>
+                        <Button variant="navigation-button" component={Link} to='/#aboutme' sx={{margin:'1rem'}}>
+                            <Typography variant="navButton">About Me</Typography>
+                        </Button>
+                        <Button variant="navigation-button" component={Link} to='/#skills' sx={{margin:'1rem'}}>
+                            <Typography variant="navButton" >Skills</Typography>
+                        </Button>
+                        <Button variant="navigation-button" component={Link} to='/#projects' sx={{margin:'1rem'}}>
+                            <Typography variant="navButton">Projects</Typography>
+                        </Button>
+                        <Button variant="navigation-button" component={Link} to='/#contactme' sx={{margin:'1rem'}}>
+                            <Typography variant="navButton">Contact <span style={{fontWeight:'700'}}>Me</span></Typography>
+                        </Button>
+                    </Box>
                 </Box>
             </Box>
         </>
